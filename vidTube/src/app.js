5734670977ff874e96c9ef7a91ab46315c7feaf7
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-25 13:07:36 +0530

    add user router in app.js

--- vidTube/src/app.js
+++ vidTube/src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
-import healthcheckRouter from "./routes/healthcheck.routes.js"; // import router
+import cookieParser from "cookie-parser";
+
+// import router
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import registerUserRouter  from "./routes/user.routes.js";
+
+
 
 const app = express()
 
@@ -17,10 +23,11 @@ app.use(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-
+app.use(cookieParser())
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/user", registerUserRouter)
 
 
 export { app }
