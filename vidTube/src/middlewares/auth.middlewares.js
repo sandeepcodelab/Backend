dbe57162c8c9721c80edf81e45eb287e6c974b75
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-29 17:33:54 +0530

    add user authentication

---
+++ vidTube/src/middlewares/auth.middlewares.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+export const verifyJWT = asyncHandler( async(req, _, next) => {
+
+    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized");
+    }
+    
+    try {
+        const decodedToekn = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToekn?._id).select("-password -refreshToken")
+        
+        if(!user){
+            throw new ApiError(401, "Unauthorized");
+        }
+        
+        req.uesr = user
+        
+        next()
+        
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid access token");
+    }
+
+})
