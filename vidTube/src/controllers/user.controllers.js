42fbaba7cedcb4ac0db5c1059757c82f3d38992a
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-30 14:49:46 +0530

    add get current user

--- vidTube/src/controllers/user.controllers.js
+++ vidTube/src/controllers/user.controllers.js
@@ -256,10 +256,18 @@ const changeCurrentPassword = asyncHandler( async(req, res) => {
                 .json( new ApiResponse(200, {}, "Password changed successfully"))
 })
 
+
+const getCurrentUser = asyncHandler( async(req, res) => {
+    
+    return res.status(200)
+                .json(new ApiResponse(200, req.user, "Current user details"))
+})
+
 export { 
     registerUser, 
     loginUser, 
     refreshAccessToken, 
     logoutUser,
-    changeCurrentPassword 
+    changeCurrentPassword,
+    getCurrentUser
 }
