b7056c520353b8166cbed75a4de3fffd7f5a0e91
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-06 12:38:21 +0530

    add get user router

--- CRUD/src/app.js
+++ CRUD/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 // Import Router
 import createUserRouter from "./routes/userRoutes.js"
+import allUsersRouter from "./routes/userRoutes.js"
+import singleUserRouter from "./routes/userRoutes.js"
 
 
 const app = express()
@@ -14,6 +16,8 @@ app.get("/", (req, res) => {
     res.send("Hello, this is CRUD operation in express JS!")
 })
 
-app.use("/api/v1/user", createUserRouter)
+app.use("/api/v1/user", createUserRouter);
+app.use("/api/v1/user", allUsersRouter);
+app.use("api/v1/user", singleUserRouter)
 
 export { app }
--- CRUD/src/routes/userRoutes.js
+++ CRUD/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, getAllUsers, singleUser } from "../controllers/userController.js";
 
 
 const router = Router();
 
 router.route("/create").post(createUser);
+router.route("/allUsers").get(getAllUsers);
+router.route("/:id").get(singleUser);
 
 
 export default router
