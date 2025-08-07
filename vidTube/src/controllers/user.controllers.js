212aae14b031ff91c118aaa2b50c12761107b08d
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-07 14:39:06 +0530

    add functionality to get subscribers profile

--- vidTube/src/controllers/user.controllers.js
+++ vidTube/src/controllers/user.controllers.js
@@ -350,6 +350,82 @@ const updateUserCoverImage = asyncHandler( async(req, res) => {
 })
 
 
+const getUserChannelProfile = asyncHandler( async(req, res) => {
+    
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is required");
+    }
+
+    const channel = await User.aggregate(
+        [
+            {
+                $match: {
+                    username: username?.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedTo"
+                }
+            },
+            {
+                $addFields: {
+                    subscribersCount: {
+                        $size: "$subscribers"
+                    },
+                    channelsSubcribedToCount: {
+                        $size: "$subscribedTo"
+                    },
+                    isSubscribed: {
+                        $cond: {
+                            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                // Project only the necessary data
+                $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                    subscribersCount: 1,
+                    channelsSubcribedToCount: 1,
+                    isSubscribed: 1,
+                    coverImage: 1
+                } 
+            }
+        ]
+    )
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel not found");
+    }
+
+    return res.status(200).json( new ApiResponse(200, channel[0], "Channel profile fetched successfully"))
+})
+
+
+const getWatchHistory = asyncHandler( async(req, res) => {
+
+})
+
+
 export { 
     registerUser, 
     loginUser, 
