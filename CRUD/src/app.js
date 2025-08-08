431f84dc2fc1f3f25d6e234481c903499780a817
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-08 12:41:17 +0530

    fix a few errors

--- CRUD/src/app.js
+++ CRUD/src/app.js
@@ -1,10 +1,7 @@
 import express from "express";
 
 // Import Router
-import createUserRouter from "./routes/userRoutes.js";
-import allUsersRouter from "./routes/userRoutes.js";
-import singleUserRouter from "./routes/userRoutes.js";
-import updateUserRouter from "./routes/userRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express()
@@ -17,9 +14,6 @@ app.get("/", (req, res) => {
     res.send("Hello, this is CRUD operation in express JS!")
 })
 
-app.use("/api/v1/user", createUserRouter);
-app.use("/api/v1/user", allUsersRouter);
-app.use("api/v1/user", singleUserRouter);
-app.use("/api/v1/user", updateUserRouter);
+app.use("/api/v1/user", userRouter);
 
 export { app }
